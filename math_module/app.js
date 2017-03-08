c1bd4afe1af103f0e01e686db6a3ef575e3a7ad5
Burton Johnsey <git@tonjohn.com>
2017-03-08 14:53:09 -0800

    add completed math_module assignment

---
+++ math_module/app.js
@@ -0,0 +1,11 @@
+var math = require('./mathlib')();
+console.log(math);
+
+console.log( "math.add(2,3):", math.add(2,3) );
+console.log( "math.multiply(3,5):", math.multiply(3,5) );
+console.log( "math.square(5):", math.square(5) );
+console.log( "math.random(1,35):", math.random(1,35) );
+console.log( "math.random(1,35):", math.random(1,35) );
+console.log( "math.random(1,35):", math.random(1,35) );
+console.log( "math.random(1,35):", math.random(1,35) );
+console.log( "math.random(1,35):", math.random(1,35) );
